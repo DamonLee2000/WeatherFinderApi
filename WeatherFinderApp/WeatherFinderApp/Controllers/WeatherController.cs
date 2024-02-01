560409eb2fb9be69ef161923a51f42055257170b
DamonLee2000 <leedamon82@gmail.com>
2024-02-01 11:52:50 -0600

    Testing GeoAPI
    Edit WeatherController.cs to test GeoAPI.
    - httpGET, jsonDeserialization
    Added \Model\GeoApiResponse
    Now using System.Text.Json

--- WeatherFinderApp/WeatherFinderApp/Controllers/WeatherController.cs
+++ WeatherFinderApp/WeatherFinderApp/Controllers/WeatherController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 using WeatherFinderApp.Data;
 using WeatherFinderApp.Models;
 
@@ -17,20 +18,58 @@ namespace WeatherFinderApp.Controllers
             _context = context;
         }
 
-        [HttpGet] // attribute specifying that this ACTION METHOD handles https GET requests.
+        [HttpPost] // attribute specifying that this ACTION METHOD. Use post because we post data from user input to be processed by my server, which will then interact with OpenWeatherMap API.
         // parameters: ZipCode and Country name from the model WeatherInput. WeatherInput is constructed through a form found in Index.cshtml
-        public IActionResult GetWeatherData([FromQuery] WeatherInput weatherInput) // this method handles GET requests to retrieve weather data.
+        public async Task<IActionResult> GetWeatherData([FromQuery] WeatherInput weatherInput) // this method handles user input to retrieve weather data.
         {
+            // get user input
             // use weatherInput to get data from the form
             var zipcode = weatherInput.Zipcode;
             var country = weatherInput.Country;
 
             var apiKey = "237653991af597a3b573e986887b2984";
-            var apiUrl = "http://api.openweathermap.org/geo/1.0/zip?zip={zip code},{country code}&appid={API key}";
+            var geoApiUrl = "http://api.openweathermap.org/geo/1.0/zip?zip={zipcode},{country}&appid={API key}";
+
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    // http get request
+                    var geoApiResposne = await httpClient.GetStringAsync(geoApiUrl); // this needs to be a string because JsonSerializer.Deserialize requries a string representing json data. change GetAsync to GetStringAsync
+
+                    if (!string.IsNullOrEmpty(geoApiResposne)) // if geoApiResponse has something in it. (geoApiResponse is a string because of the GetStringAsync method).
+                    {
+                        var geoApiData = JsonSerializer.Deserialize<List<GeoApiResponse>>(geoApiResposne);
+
+                        if (geoApiData.Count > 0)
+                        {
+                            var latitude = geoApiData[0].Latitude;
+                            var longitude = geoApiData[0].Longitude;
+
+                            Console.WriteLine(latitude);
+                            Console.WriteLine(longitude);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("GeoAPI response is empty");
+                    }
+                }
+            }
+            catch (HttpRequestException ex) // error http GET
+            {
+                Console.WriteLine("HTTP GET ERROR: {ex.Message}");
+                return BadRequest("Error getting data from OpenWeatherMap API");
+            }
+            catch (JsonException ex) // error deserializing API response
+            {
+                Console.WriteLine("JSON deserialization error: {ex.Message}");
+                return BadRequest("Error parsing JSON string");
+            }
 
             // database interaction:
-            var weatherData = _context.WeatherTable.ToList(); // uses "_context" (instance of WeatherDbContext) to query database for all entries in WeatherTable. Converts results to a list.
-            return Ok(weatherData); // returns HTTP 200 ok response and weather data.
+            //var weatherData = _context.WeatherTable.ToList(); // uses "_context" (instance of WeatherDbContext) to query database for all entries in WeatherTable. Converts results to a list.
+            return Ok("done"); // returns HTTP 200 ok response and weather data.
         }
     }
 }
---
+++ WeatherFinderApp/WeatherFinderApp/Models/GeoApiResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace WeatherFinderApp.Models
+{
+    public class GeoApiResponse
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
